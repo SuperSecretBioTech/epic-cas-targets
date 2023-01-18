ae27ab84639904c53f174801c961edcef506a415
Nishant <me@nishantjha.org>
2023-01-18 13:59:07 -0800

    first pass table

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from "next";
 import Shell from "../components/Shell";
+import { Table } from "../components/Table";
 
 const Results: NextPage = () => {
   return (
     <Shell>
-      <div className="grid grid-cols-12 gap-8">Hello World</div>
+      <div className="flex flex-col gap-8">
+        <div className="rounded-xl bg-white px-8 py-6">
+          <div className="rounded-lg bg-white px-5 py-6 shadow dark:bg-zinc-600 sm:px-6 ">
+            <div className="grid h-60 content-center rounded-lg border-4 border-dashed border-gray-200">
+              <h1 className="text-center text-7xl font-semibold text-zinc-300">
+                Visualization
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="rounded-xl bg-white px-8 py-6">
+          <Table />
+        </div>
+      </div>
     </Shell>
   );
 };
