2921825b94e952e44380c1ea4d071c59ff31095b
Nishant <me@nishantjha.org>
2023-01-18 14:09:05 -0800

    generate mock data helper util

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,22 +1,35 @@
 import type { NextPage } from "next";
 import Shell from "../components/Shell";
-import { Table } from "../components/Table";
+import { Table, TableData } from "../components/Table";
+import { generateMockTableData } from "../utils/tableUtils";
 
-const Results: NextPage = () => {
+export async function getStaticProps() {
+  const data = generateMockTableData(100);
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+const Results: NextPage<{ data: TableData[] }> = ({
+  data,
+}: {
+  data: TableData[];
+}) => {
   return (
     <Shell>
       <div className="flex flex-col gap-8">
-        <div className="rounded-xl bg-white px-8 py-6">
-          <div className="rounded-lg bg-white px-5 py-6 shadow dark:bg-zinc-600 sm:px-6 ">
-            <div className="grid h-60 content-center rounded-lg border-4 border-dashed border-gray-200">
-              <h1 className="text-center text-7xl font-semibold text-zinc-300">
-                Visualization
-              </h1>
-            </div>
+        <div className="rounded-lg bg-white px-5 py-6 shadow dark:bg-zinc-600 sm:px-6 ">
+          <div className="grid h-60 content-center rounded-lg border-4 border-dashed border-gray-200">
+            <h1 className="text-center text-xl font-semibold text-zinc-300 md:text-4xl lg:text-5xl">
+              Visualization
+            </h1>
           </div>
         </div>
         <div className="rounded-xl bg-white px-8 py-6">
-          <Table />
+          <Table data={data} />
         </div>
       </div>
     </Shell>
