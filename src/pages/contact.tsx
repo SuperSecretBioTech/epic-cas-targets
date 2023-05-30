b280e1af1dad0ccff586c3ae062ba5bed582424f
Nishant <me@nishantjha.org>
2023-05-30 13:57:52 -0700

    consolidate contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -15,82 +15,9 @@ const Contact: NextPage = () => {
             Contact
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            For more information, contact tim.daley@epic-bio.com.
+            For more information, contact{" "}
+            <a href="mailto:tim.daley@epic-bio.com">tim.daley@epic-bio.com</a>
           </p>
-        </div>
-        <div className="mx-auto mt-20 max-w-lg space-y-16">
-          <div className="flex gap-x-6">
-            <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-brand-600">
-              <ChatBubbleLeftRightIcon
-                className="h-6 w-6 text-white"
-                aria-hidden="true"
-              />
-            </div>
-            <div>
-              <h3 className="text-base font-semibold leading-7 text-gray-900">
-                Sales support
-              </h3>
-              <p className="mt-2 leading-7 text-gray-600">
-                Ut cursus est ut amet. Lobortis eget egestas leo vitae eget
-                porttitor risus blandit. Nunc a in lorem vel iaculis porttitor.
-              </p>
-              <p className="mt-4">
-                <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 text-brand-600"
-                >
-                  Contact us <span aria-hidden="true">&rarr;</span>
-                </a>
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-x-6">
-            <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-brand-600">
-              <BugAntIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </div>
-            <div>
-              <h3 className="text-base font-semibold leading-7 text-gray-900">
-                Bug reports
-              </h3>
-              <p className="mt-2 leading-7 text-gray-600">
-                Expedita qui non ut quia ipsum voluptatum ipsam pariatur. Culpa
-                vitae ipsum minus eius vero quo quibusdam.
-              </p>
-              <p className="mt-4">
-                <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 text-brand-600"
-                >
-                  Report a bug <span aria-hidden="true">&rarr;</span>
-                </a>
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-x-6">
-            <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-brand-600">
-              <ComputerDesktopIcon
-                className="h-6 w-6 text-white"
-                aria-hidden="true"
-              />
-            </div>
-            <div>
-              <h3 className="text-base font-semibold leading-7 text-gray-900">
-                Technical support
-              </h3>
-              <p className="mt-2 leading-7 text-gray-600">
-                Sint aut modi porro consequatur architecto commodi qui
-                consequatur. Dignissimos adipisci minima.
-              </p>
-              <p className="mt-4">
-                <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 text-brand-600"
-                >
-                  Join our Discord <span aria-hidden="true">&rarr;</span>
-                </a>
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </Shell>
