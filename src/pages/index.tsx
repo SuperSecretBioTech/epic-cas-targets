37acefc43817323fff31dfb80432899592cbf197
spencer-lopp <102998727+spencer-lopp@users.noreply.github.com>
2023-05-30 10:51:38 -0700

    test amplify hosting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ const Home: NextPage = () => {
       <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
         <div className="grid grid-cols-12 gap-8">
           <div className="col-span-12">
+            ...terrible things
             <FormInput />
           </div>
         </div>
