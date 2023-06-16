02b52527735b070af8a0ec603b17d0b6b328c2ca
Spencer Lopp <spencerlopp@Spencer-Lopps-MacBook-Pro.local>
2023-06-16 10:03:32 -0700

    add hyperlink to paper

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -19,7 +19,7 @@ const About: NextPage = () => {
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
             {`We designed a spacer design tool that allows users to retrieve genome-wide activation and
-            suppression guides for nuclease inactivated dCasMINI (Xu et al., 2021).
+            suppression guides for nuclease inactivated dCasMINI [(Xu et al., 2021)](https://www.cell.com/molecular-cell/pdfExtended/S1097-2765(21)00648-1).
             This tool should facilitate epigenetic modulation studies involving dCasMINI (or other dCas molecules recognizing TTTR PAMs)
             by computing genome-wide activation/suppression guides that can be queried by users based on gene IDs. Compared to dCas9, 
             dCasMINI is a fraction of the size, facilitating therapeutic delivery, and has a favorable off-target profile [(Xin et al., 2022)](https://www.nature.com/articles/s41467-022-33346-1#article-info).`}
