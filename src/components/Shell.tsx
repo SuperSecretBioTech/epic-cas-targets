e5401bccb0be6ccfef8aa95b4d67031f6c3c7990
Spencer Lopp <spencerlopp@Spencer-Lopps-MacBook-Pro.local>
2023-06-16 10:53:07 -0700

    change name for branding accuracy

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -125,7 +125,7 @@ function Header() {
       <header className="py-10">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-white">
-            CasMini Guide Design Tool
+            dCasMINI Guide Design Tool
           </h1>
         </div>
       </header>
